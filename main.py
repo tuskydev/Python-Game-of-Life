2ef11ccefc0aca4aa12e4a3051caf533d8975477
Mario Fernandez <diddleslip@gmail.com>
2023-11-28 18:13:23 -0500

    Added population tracker

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ length = (len(listofLists[0])) ## Length; from left to right
 width = (len(listofLists)) ## Width; from top to bottom
 count = 0
 
-# Set x and y ticks dynamically based on the data size
 plt.figure(figsize=(9, 8.5))
+# Set x and y ticks dynamically based on the data size
 xticks = [i-0.5 for i in range(width)]
 yticks = [i-0.525 for i in range(length)]
 plt.xticks(xticks, labels="")
 plt.yticks(yticks, labels="")
-plt.imshow(listofLists, cmap='gray', interpolation='nearest')
+plt.imshow(listofLists, cmap='gray')
 plt.grid()
-plt.xlabel(f"Ticks: {count}")
+plt.xlabel(f"Ticks: {count}               \
+           Population count: 25")
 plt.ylabel("")
 plt.title("Game of Life")
 # plt.legend()
