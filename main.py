5d555bd314faf695881acf487a6236cf00d239db
Mario Fernandez <diddleslip@gmail.com>
2023-11-28 18:05:41 -0500

    List of lists aligned with grid graph

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import matplotlib.pyplot as plt
 import random
 
-newL = [[random.choice([0, 1]) for _ in range(5)] for _ in range(5)]
+listofLists = [[random.choice([0, 1]) for _ in range(20)] for _ in range(20)]
+length = (len(listofLists[0])) ## Length; from left to right
+width = (len(listofLists)) ## Width; from top to bottom
+count = 0
 
-# for list in newL:
-#   print(list)
-
-print(newL)
-
+# Set x and y ticks dynamically based on the data size
+plt.figure(figsize=(9, 8.5))
+xticks = [i-0.5 for i in range(width)]
+yticks = [i-0.525 for i in range(length)]
+plt.xticks(xticks, labels="")
+plt.yticks(yticks, labels="")
+plt.imshow(listofLists, cmap='gray', interpolation='nearest')
+plt.grid()
+plt.xlabel(f"Ticks: {count}")
+plt.ylabel("")
+plt.title("Game of Life")
+# plt.legend()
+plt.show()
