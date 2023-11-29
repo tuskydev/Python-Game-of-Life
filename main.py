aacea00ee31ee0195c7ce2351f87bea0b7f8988d
Mario Fernandez <diddleslip@gmail.com>
2023-11-28 20:01:45 -0500

    Polished up tiny details on legend chart

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 import random
 
+
 listofLists = [[random.choice([0, 1]) for _ in range(20)] for _ in range(20)]
+# listofLists = [[1, 1], [1, 1]]
 length = (len(listofLists[0])) ## Length; from left to right
 width = (len(listofLists)) ## Width; from top to bottom
 count = 0
@@ -12,11 +15,13 @@ xticks = [i-0.5 for i in range(width)]
 yticks = [i-0.525 for i in range(length)]
 plt.xticks(xticks, labels="")
 plt.yticks(yticks, labels="")
-plt.imshow(listofLists, cmap='gray')
+plt.imshow(listofLists, cmap=ListedColormap(['#000000', '#ffffff']))
 plt.grid()
 plt.xlabel(f"Ticks: {count}               \
-           Population count: 25")
+           Population count: 1000")
 plt.ylabel("")
 plt.title("Game of Life")
-# plt.legend()
+legendHandles = [plt.Line2D([0], [0], color="w", marker="s", markersize=12, markerfacecolor="#000000", label="Black= Alive"),
+                 plt.Line2D([0], [0], color="k", marker="s", markersize=12, markerfacecolor="#ffffff", label="White = Dead")]
+plt.legend(handles=legendHandles, loc="upper right")
 plt.show()
