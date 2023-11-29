0363589a9e5f5ac249f4cd115a89df2cd4534df8
Mario Fernandez <diddleslip@gmail.com>
2023-11-29 12:08:51 -0500

    Added function comments

--- main.py
+++ main.py
@@ -2,14 +2,28 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import random
 
+"""
+Rules:
+
+1. Any live cell with two or three live neighbours survives.
+2. Any dead cell with three live neighbours becomes a live cell.
+3. All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+"""
+
 # listofLists = [[1, 1], [1, 1]]
 listofLists = [[random.choice([0, 1]) for _ in range(20)] for _ in range(20)]
 length = (len(listofLists[0])) ## Length; from left to right
 width = (len(listofLists)) ## Width; from top to bottom
-count = 0
+tick = 0
 
-## Implement populationCount function
 def populationCount(lists):
+  """
+  lists: a list of lists
+
+  Calculates how many live cells there are in the current tick
+
+  Returns an int
+  """
   counter = 0
 
   for list in lists:
@@ -17,9 +31,35 @@ def populationCount(lists):
 
   return counter
 
-print(populationCount(listofLists))
+def doesClear():
+  """
 
 
+  Returns a boolean
+  """
+
+
+
+
+
+
+def birthCell(list, ):
+  """
+
+  Returns a boolean
+  """
+
+
+
+
+
+
+def update():
+  """
+
+  Updates
+  Returns a new list of lists holding all the cells
+  """
 
 
 
@@ -37,7 +77,7 @@ plt.xticks(xticks, labels="")
 plt.yticks(yticks, labels="")
 plt.imshow(listofLists, cmap=ListedColormap(['#000000', '#ffffff']))
 plt.grid()
-plt.xlabel(f"Ticks: {count}               \
+plt.xlabel(f"Ticks: {tick}               \
            Population count: 1000")
 plt.ylabel("")
 plt.title("Game of Life")
