3c63239f904bac9ad2a4155068fddfb3954545ed
Mario Fernandez <diddleslip@gmail.com>
2023-11-29 19:19:41 -0500

    141 lines of code later, we have reached the finish line!

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 from matplotlib.colors import ListedColormap
 import random
 
@@ -12,12 +13,12 @@ Rules:
 """
 
 # listofLists = [[1, 0], [1, 1]]
-listofLists = [[random.choice([0, 1]) for _ in range(4)] for _ in range(4)]
-row = (len(listofLists[0])) ## Left to right
-column = (len(listofLists)) ## Top to bottom
+listofLists = [[random.choice([0, 1]) for _ in range(15)] for _ in range(15)]
+row = (len(listofLists[0])) # Left to right
+column = (len(listofLists)) # Top to bottom
 tick = 0
 
-def populationCount(lists):
+def getPopCount(lists):
   """
   Calculates how many live cells there are in the current tick
 
@@ -45,77 +46,96 @@ def getLiveNeighbors(lists, cell):
   Returns:
   int: The number of alive neighboring cells.
   """
-  cellNeighbors = 0
   prevRow = []
   currRow = lists[cell[0]]
   nextRow = []
 
-  ## If a cell is at the top wall
-  if cell[0] > 0:
+  # Extract columns based on cell's position
+  if cell[0] > 0: # Top wall
     prevRow = lists[cell[0]-1]
-  ## If a cell is at the bottom wall
-  if cell[0]+1 <= row-1:
+  if cell[0]+1 <= row-1: # Bottom wall
     nextRow = lists[cell[0]+1]
 
-
-  ## If a cell is at the left wall
-  if cell[1] == 0:
+  # Extract columns based on cell's position
+  if cell[1] == 0: # Left wall
     prevRow = prevRow[:2]
     currRow = currRow[1:2]
     nextRow = nextRow[:2]
-  ## If a cell is at the right wall
-  elif cell[1] == column-1:
+  elif cell[1] == column-1: # Right wall
     prevRow = prevRow[cell[1]-1:column]
     currRow = currRow[cell[1]-1:column-1]
     nextRow = nextRow[cell[1]-1:column]
-  ## # If a cell is in the middle
-  else:
+  else: # Cell is in the middle
     prevRow = prevRow[cell[1]-1:cell[1]+2]
     currRow = currRow[cell[1]-1:cell[1]+2:2]
     nextRow = nextRow[cell[1]-1:cell[1]+2]
 
-  cellNeighbors += sum(prevRow) + sum(currRow) + sum(nextRow)
+  return sum(prevRow) + sum(currRow) + sum(nextRow)
 
-  return cellNeighbors
-
-cell = (1, 1)
-print(getLiveNeighbors(listofLists, cell))
-
-
-def update():
+def update(lists):
   """
   Updates the game by one tick
 
   Parameters:
-  -
+  - lists (list): a list of lists
 
   Returns:
-  None
+  updatedList (list): a list of lists
   """
+  global tick
+  tick += 1
+  updatedList = lists
 
+  for rowIndex, list in enumerate(lists):
+    for colIndex, _ in enumerate(list):
+      cell = (rowIndex, colIndex)
+      neighbors = getLiveNeighbors(lists, cell)
 
+      # Cells with fewer than two live neighbours dies
+      if neighbors < 2:
+        updatedList[rowIndex][colIndex] = 0
+      # Cells with fewer than two live neighbours dies
+      elif neighbors == 2 and updatedList[rowIndex][colIndex] == 1:
+        updatedList[rowIndex][colIndex] == 1
+      # Cells with exactly three live neighbours becomes a live cell
+      elif neighbors == 3:
+        updatedList[rowIndex][colIndex] = 1
+      # Cells with more than three live neighbours dies
+      elif neighbors > 3:
+        updatedList[rowIndex][colIndex] = 0
 
+  return updatedList
 
+def plotGame(ax, lists):
+  ax.set_xlabel(f"Ticks: {tick}\
+              Population count: {getPopCount(lists)}")
 
+def animate():
+  global listofLists
 
+  _, ax = plt.subplots(figsize=(9, 8.5))
+  xticks = [i - 0.5 for i in range(column)]
+  yticks = [i - 0.51 for i in range(row)]
+  ax.set_xticks(xticks)
+  ax.set_xticklabels(labels="")
+  ax.set_yticks(yticks)
+  ax.set_yticklabels(labels="")
+  ax.grid()
+  ax.set_ylabel("")
+  ax.set_title("Game of Life")
+  legendHandles = [plt.Line2D([0], [0], marker="s", markersize=12, markerfacecolor="#000000",
+                              color="w", label="Black= Alive"),
+                  plt.Line2D([0], [0], marker="s", markersize=12, markerfacecolor="#ffffff",
+                              color="k", label="White = Dead")]
+  ax.legend(handles=legendHandles, loc="upper right")
+  img = ax.imshow(listofLists, cmap=ListedColormap(['#ffffff', '#000000']))
 
+  while getPopCount(listofLists) > 0:
+    listofLists = update(listofLists)
+    ax.set_xlabel(f"Ticks: {tick}       \
+                  Population count: {getPopCount(listofLists)}")
+    img.set_data(listofLists)
+    plt.pause(0.01)
 
-
-plt.figure(figsize=(9, 8.5))
-# Set x and y ticks dynamically based on the data size
-xticks = [i-0.5 for i in range(column)]
-yticks = [i-0.51 for i in range(row)]
-plt.xticks(xticks, labels="")
-plt.yticks(yticks, labels="")
-plt.imshow(listofLists, cmap=ListedColormap(['#ffffff', '#000000']))
-plt.grid()
-plt.xlabel(f"Ticks: {tick}               \
-           Population count: 1000")
-plt.ylabel("")
-plt.title("Game of Life")
-legendHandles = [plt.Line2D([0], [0], marker="s", markersize=12, markerfacecolor="#000000",
-                            color="w", label="Black= Alive"),
-                 plt.Line2D([0], [0], marker="s", markersize=12, markerfacecolor="#ffffff",
-                            color="k", label="White = Dead")]
-plt.legend(handles=legendHandles, loc="upper right")
+animate()
 plt.show()
